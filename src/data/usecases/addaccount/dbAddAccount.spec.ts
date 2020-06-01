d659a218a47767b2b2055e77321bfcee3ef2ca91
Pavan <rogerio_pavan@hotmail.com>
2020-06-01 20:25:30 -0300

    refactor: move a criação do Encrypter para um helper

--- src/data/usecases/addaccount/dbAddAccount.spec.ts
+++ src/data/usecases/addaccount/dbAddAccount.spec.ts
@@ -6,14 +6,17 @@ interface SutTypes {
   encrypterStube: Encrypter
 }
 
-const makeSut = (): any => {
-  class EncrypterStub {
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
     async encrypt (value: string): Promise<string> {
       return 'senha_encriptografada'
     }
   }
+  return new EncrypterStub()
+}
 
-  const encrypterStub = new EncrypterStub()
+const makeSut = (): any => {
+  const encrypterStub = makeEncrypter()
   const sut = new DbAddAccount(encrypterStub)
 
   return {
