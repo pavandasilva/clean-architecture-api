ed443756c3cc87c20edde38cad8b1c11040277e1
Pavan <rogerio_pavan@hotmail.com>
2020-06-01 02:30:42 -0300

    refactor: cria o metodo httpHelper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,20 +1,15 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import MissingParamError from '../errors/missingParamError'
+import { badRequest } from '../helpers/httpHelper'
 
 class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.nome) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('nome')
-      }
+      return badRequest(new MissingParamError('nome'))
     }
 
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
