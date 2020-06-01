8e9cd39124a3a6aa6c0214432e0ec549a83afe84
Pavan <rogerio_pavan@hotmail.com>
2020-06-01 02:08:09 -0300

    feat: retorna erro quando o email  não é informado

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,17 @@
 class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Faltando o parâmetro nome')
+    if (!httpRequest.body.nome) {
+      return {
+        statusCode: 400,
+        body: new Error('Faltando o parâmetro nome')
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Faltando o parâmetro email')
+      }
     }
   }
 }
