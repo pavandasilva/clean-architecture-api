39d009ddc1991b295227139e3d682edf38057f03
Pavan <rogerio_pavan@hotmail.com>
2020-06-01 21:56:10 -0300

    feat: chama o bcrypt com o valor correto

---
+++ src/infra/criptography/bcryptAdapter.ts
@@ -0,0 +1,15 @@
+import { hash } from 'bcrypt'
+import { Encrypter } from '../../data/protocols/encrypter'
+
+export class BcryptAdapter implements Encrypter {
+  private readonly salt: number
+
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
+  async encrypt (value: string): Promise<string> {
+    await hash(value, 12)
+    return ''
+  }
+}
