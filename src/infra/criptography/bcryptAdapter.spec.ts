2d746bb5e31c6c11bd67287e79b2bb5f46e79f44
Pavan <rogerio_pavan@hotmail.com>
2020-06-01 22:05:03 -0300

    test: retorna a hash gerada no bcrypt

--- src/infra/criptography/bcryptAdapter.spec.ts
+++ src/infra/criptography/bcryptAdapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcryptAdapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return 'hash'
+  }
+}))
+
 describe('bcryptAdapter', () => {
   test('deve chamar o bcrypt com o valor correto', async () => {
     const bcryptSalt = 12
@@ -9,5 +15,13 @@ describe('bcryptAdapter', () => {
 
     await sut.encrypt('senha')
     expect(hashSpy).toHaveBeenCalledWith('senha', bcryptSalt)
+  })
+
+  test('deve retornar a hash gerada no bcrypt', async () => {
+    const bcryptSalt = 12
+    const sut = new BcryptAdapter(bcryptSalt)
+
+    const hash = await sut.encrypt('senha')
+    expect(hash).toBe('hash')
   })
 })
