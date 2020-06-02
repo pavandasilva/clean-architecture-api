0e0db4e07a26234e835cb678185655ea71370ef1
Pavan <rogerio_pavan@hotmail.com>
2020-06-01 21:18:49 -0300

    feat: chama  AddAccountRepository com os valores corretos

--- src/data/usecases/addaccount/dbAddAccount.ts
+++ src/data/usecases/addaccount/dbAddAccount.ts
@@ -1,13 +1,17 @@
-import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './dbAddAccountProtocols'
+import { AddAccount, AddAccountModel, AccountModel, Encrypter, AddAccountRepository } from './dbAddAccountProtocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
-  constructor (encrypter: Encrypter) {
+  private readonly addAccountRepository: AddAccountRepository
+
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.senha)
+    const hashedSenha = await this.encrypter.encrypt(account.senha)
+    await this.addAccountRepository.add({ ...account, senha: hashedSenha })
     return null
   }
 }
