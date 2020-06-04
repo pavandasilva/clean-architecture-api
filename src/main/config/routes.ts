be929cddf847fc3162fe5a06481e31c306bf4f9b
Pavan <rogerio_pavan@hotmail.com>
2020-06-04 00:38:20 -0300

    refactor: conecta no MongoDb antes de cada teste de integração

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -3,8 +3,8 @@ import fastGlob from 'fast-glob'
 
 export default (app: Express): void => {
   const router = Router()
-
   app.use('/api', router)
+
   fastGlob
     .sync('**/src/main/routes/**Routes.ts')
     .map(async (file) => (await import(`../../../${file}`)).default(router))
