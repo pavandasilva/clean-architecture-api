204fb932c95f6ccb81911a16062955521c936900
Pavan <rogerio_pavan@hotmail.com>
2020-06-05 01:54:11 -0300

    refactor: adiciona função para criar sut

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,27 +1,41 @@
 import { LogControllerDecorator } from './log'
 import { HttpResponse, HttpRequest, Controller } from '../../presentation/protocols'
 
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): any => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'myname',
+          email: 'myemail@email.com',
+          password: '1234',
+          passwordConfirmation: '1234'
+        }
+      }
+      return httpResponse
+    }
+  }
+
+  const controllerStub = new ControllerStub()
+
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('LogController Decorator', () => {
   test('deve garantir que está repassando a chamada', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'myname',
-            email: 'myemail@email.com',
-            password: '1234',
-            passwordConfirmation: '1234'
-          }
-        }
-        return httpResponse
-      }
-    }
-
-    const controllerStub = new ControllerStub()
+    const { controllerStub, sut } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
 
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest = {
       body: {
         name: 'myname',
