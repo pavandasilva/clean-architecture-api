fb39fec706fb0415728ad32a17088af44d7e602f
Pavan <rogerio_pavan@hotmail.com>
2020-06-05 01:30:17 -0300

    test: garante que está repassando a chamada

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,13 +3,17 @@ import { EmailValidatorAdapter } from '../../utils/emailValidatorAdapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/accountRepository/acccount'
 import { DbAddAccount } from '../../data/usecases/addaccount/dbAddAccount'
 import { BcryptAdapter } from '../../infra/criptography/bcryptAdapter'
+import { Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
 
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+
+  return new LogControllerDecorator(signUpController)
 }
